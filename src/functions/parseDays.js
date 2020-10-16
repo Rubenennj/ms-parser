a39bb41494bfc111ad0e585318ee49b2cb9a90f5
Rubenennj <rubenmudarrajurado@hotmail.com>
2020-10-16 15:40:40 +0200

    fixed stuff

--- src/functions/parseDays.js
+++ src/functions/parseDays.js
@@ -4,6 +4,16 @@ const defineValue = require("./defineValue")
 
 module.exports = (days) => {
 
+    const timeValues = [
+        defineValue("s", "second", valuesMS.seconds, 60),
+        defineValue("m", "minute", valuesMS.minutes, 60),
+        defineValue("h", "hour", valuesMS.hours, 24),
+        defineValue("d", "day", valuesMS.days, 7),
+        defineValue("w", "week", valuesMS.weeks, 4),
+        defineValue("M", "month", valuesMS.months, 12),
+        defineValue("y", "year", valuesMS.years, 100)
+    ]
+    
     const utils = timeValues
 
     const container = new Array()
